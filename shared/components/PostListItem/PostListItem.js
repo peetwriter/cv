6c710cd6c13447b3108d44221a419d88621f7d57
Petro Tarasenko <pereter92@gmail.com>
2016-02-23 18:58:38 +0200

    postList item changes

--- shared/components/PostListItem/PostListItem.js
+++ shared/components/PostListItem/PostListItem.js
@@ -3,27 +3,26 @@
 import React, { PropTypes, Component } from 'react';
 import { Link } from 'react-router';
 
-function PostListItem(props, context) {
+function WorkPlaceListItem(props, context) {
   return (
     <div className="single-post">
       <h3 className="post-title ">
-        <Link to={'/post/' + props.post.slug + '-' + props.post.cuid} onClick={props.onClick}>
-            {props.post.title}
+        <Link to={'/workPlace/' + props.workPlace.slug + '-' + props.workPlace.cuid} onClick={props.onClick}>
+            {props.workPlace.title}
         </Link>
       </h3>
-      <p className="author-name">By {props.post.name}</p>
-      <p className="post-desc">{props.post.content}</p>
-      <p className="post-action"><a href="#" onClick={props.onDelete}>Delete Post</a></p>
+      <p className="post-desc">{props.workPlace.content}</p>
+      <p className="post-action"><a href="#" onClick={props.onDelete}>Delete WorkPlace</a></p>
       <hr className="divider"/>
     </div>
   );
 }
 
-PostListItem.propTypes = {
+WorkPlaceListItem.propTypes = {
   post: PropTypes.shape({
-    name: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     content: PropTypes.string.isRequired,
+    startDate: PropTypes.string.isRequired,
     slug: PropTypes.string.isRequired,
     cuid: PropTypes.string.isRequired,
   }).isRequired,
@@ -32,4 +31,4 @@ PostListItem.propTypes = {
   onDelete: PropTypes.func.isRequired,
 };
 
-export default PostListItem;
+export default WorkPlaceListItem;
